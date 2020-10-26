4b2561b433eee62516b0963a7c3d3982a363fc14
Alex <70995153+AlexBentov@users.noreply.github.com>
2020-10-26 16:21:34 +0800

    Update account-store-api.ts

--- packages/apps-electron/src/api/account-store-api.ts
+++ packages/apps-electron/src/api/account-store-api.ts
@@ -1,6 +1,7 @@
 // Copyright 2017-2020 @polkadot/apps authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+
 import { KeyringJson } from '@polkadot/ui-keyring/types';
 
 export interface AccountStoreApi {
